7d84d1f4d82b7e62c813b3cbfc4ce0b9aba468ed
maysonsilv <mayson.sillva@gmail.com>
2021-08-28 23:38:54 -0300

    Desenvolvendo o ContactAdapter
    Desenvolvendo a classe ContactAdapter com as informações de cada card e estilizando o mesmo

--- app/src/main/java/com/mayson/listadecontatossimples/ContactAdapter.kt
+++ app/src/main/java/com/mayson/listadecontatossimples/ContactAdapter.kt
@@ -3,31 +3,48 @@ package com.mayson.listadecontatossimples
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.view.menu.ActionMenuItemView
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class ContactAdapter: RecyclerView.Adapter<ContactAdapter.ContactViewHolder>(){
+class ContactAdapter: RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHolder>(){
 
     private val list: MutableList<Contact> = mutableListOf()
 
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
-       var view = LayoutInflater.from(parent.context).inflate(R.layout.contact_item, parent, false)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapterViewHolder {
+       val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_item, parent, false)
 
-       return ContactViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
-        holder.find(list[position])
+       return ContactAdapterViewHolder(view)
     }
 
     override fun getItemCount(): Int {
         return list.size
     }
 
-    class ContactViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
 
-        fun find(contact: Contact){}
+    override fun onBindViewHolder(holder: ContactAdapterViewHolder, position: Int) {
+        holder.bind(list[position])
+    }
+
+
+    fun updateList(list: List<Contact>){
+        this.list.clear()
+        this.list.addAll(list)
+
+    }
+
+    class ContactAdapterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        private val tvNome: TextView = itemView.findViewById(R.id.tv_nome)
+        private val tvTelefone: TextView = itemView.findViewById(R.id.tv_telefone)
+        private val tvImagem: ImageView = itemView.findViewById(R.id.iv_imagem)
+
+        fun bind(contact: Contact){
+            tvNome.text = contact.nome
+            tvTelefone.text = contact.telefone
+
+        }
     }
 
 
--- app/src/main/java/com/mayson/listadecontatossimples/MainActivity.kt
+++ app/src/main/java/com/mayson/listadecontatossimples/MainActivity.kt
@@ -2,10 +2,46 @@ package com.mayson.listadecontatossimples
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private val rvList: RecyclerView by lazy {
+        findViewById<RecyclerView>(R.id.rv_list)
+    }
+
+    private val adapter = ContactAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bindViews()
+        updateList()
+    }
+
+
+    private fun bindViews(){
+        rvList.adapter = adapter
+        rvList.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun updateList(){
+        adapter.updateList(
+            arrayListOf(
+                Contact(
+                    "Meyson Silva",
+                    "(00) 0000-0000",
+                    "img.png"
+
+                ),
+
+                Contact(
+                    "Fulano da Silva",
+                    "(11) 1111-1111",
+                    "img.jpg"
+                )
+            )
+        )
     }
 }
