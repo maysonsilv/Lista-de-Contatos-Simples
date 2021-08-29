3d953bd5dc1f9aaa29f721c8eea10326769778db
maysonsilv <mayson.sillva@gmail.com>
2021-08-29 14:46:01 -0300

    Criando Menu
    Desenvolvendo Options Menus com dois itens retornando um Toast

--- app/src/main/java/com/mayson/listadecontatossimples/MainActivity.kt
+++ app/src/main/java/com/mayson/listadecontatossimples/MainActivity.kt
@@ -2,6 +2,10 @@ package com.mayson.listadecontatossimples
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -40,8 +44,51 @@ class MainActivity : AppCompatActivity() {
                     "Fulano da Silva",
                     "(11) 1111-1111",
                     "img.jpg"
+                ),
+
+                Contact(
+                    "Cybele",
+                    "(98) 98483-4018",
+                    "cy.img"
+                ),
+
+                Contact(
+                    "Pai",
+                    "(99) 99177-0775",
+                    "imagen.png",
+
+                ),
+                Contact(
+                    "Mãe",
+                    "(98) 99891-0909",
+                    "foto.png"
                 )
             )
         )
+    }
+
+    private fun showToast(message: String){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT ).show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.item_menu_1 -> {
+                showToast("Exibindo item de menu 1")
+                return true
+
+            }
+            R.id.item_menu_2 -> {
+                showToast("Exibindo item de menu 2")
+                return true
+            }
+            else -> onOptionsItemSelected(item)
+        }
     }
 }
