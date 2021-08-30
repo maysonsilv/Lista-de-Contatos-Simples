ff15e1c70e3a9dc340d521a0d8846af7377003e8
maysonsilv <mayson.sillva@gmail.com>
2021-08-29 22:53:32 -0300

    Armazenando e Acessando dados
    Armazenando e Acessando dados em SharedPreferences

--- app/src/main/java/com/mayson/listadecontatossimples/MainActivity.kt
+++ app/src/main/java/com/mayson/listadecontatossimples/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.mayson.listadecontatossimples
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -10,9 +12,12 @@ import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.edit
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import com.mayson.listadecontatossimples.DetailActivity.Companion.EXTRA_CONTACT
 
 
@@ -28,31 +33,14 @@ class MainActivity : AppCompatActivity(), ClickItemContactListener {
         setContentView(R.layout.drawer)
 
         initDrawer()
+        fetchListContact()
         bindViews()
-        updateList()
+
+
     }
 
-    private fun initDrawer() {
-        val drawerLayout = findViewById<View>(R.id.drawer_layout) as DrawerLayout
-
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
-
-
-        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer)
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
-    }
-
-
-    private fun bindViews(){
-        rvList.adapter = adapter
-        rvList.layoutManager = LinearLayoutManager(this)
-    }
-
-    private fun updateList(){
-        adapter.updateList(
-            arrayListOf(
+    private fun fetchListContact(){
+        var list = arrayListOf(
                 Contact(
                     "Meyson Silva",
                     "(00) 0000-0000",
@@ -77,14 +65,64 @@ class MainActivity : AppCompatActivity(), ClickItemContactListener {
                     "(99) 99177-0775",
                     "imagen.png",
 
-                ),
+                    ),
                 Contact(
                     "Mãe",
                     "(98) 99891-0909",
                     "foto.png"
                 )
-            )
+
         )
+
+        getInstanceSharedPreferences().edit{
+            val json = Gson().toJson(list)
+            putString("contacts", json)
+            commit()
+        }
+
+
+
+        }
+
+    private fun getInstanceSharedPreferences(): SharedPreferences {
+        return getSharedPreferences("com.mayson.listadecontatossimples.PREFERENCES", Context.MODE_PRIVATE)
+    }
+
+
+
+
+    private fun initDrawer() {
+        val drawerLayout = findViewById<View>(R.id.drawer_layout) as DrawerLayout
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
+
+    private fun bindViews(){
+        rvList.adapter = adapter
+        rvList.layoutManager = LinearLayoutManager(this)
+        updateList()
+    }
+
+    private fun getListContacts(): List<Contact> {
+
+        val list = getInstanceSharedPreferences().getString("contacts", "[]")
+        val turnType = object : TypeToken<List<Contact>>() {}.type
+        return Gson().fromJson(list, turnType)
+            
+        }
+
+
+    private fun updateList(){
+        val list = getListContacts()
+        adapter.updateList(list)
+
     }
 
     private fun showToast(message: String){
